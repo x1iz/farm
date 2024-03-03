77cde99569e63f262af2e3b2d6a28f0795970c92
x1iz <162029968+x1iz@users.noreply.github.com>
2024-03-03 16:35:59 +0300

    Update loader.lua

--- loader.lua
+++ loader.lua
@@ -1,18 +1,1 @@
-_G.AutofarmSettings = {
-    ["AttackMode"] = "3",
-
-    ["Fps"] = "60",
-
-    ["Codes"] = {
-        "1",
-        "2"
-    },
-
-    ["Webhook"] = "grr",
-
-    ["LogInterval"] = "60",
-
-    ["Credits"] = "nikzittxe's autofarm - by @hea3i"
-}
-
-loadstring(game:HttpGet("https://raw.githubusercontent.com/x1iz/farm/main/main.lua"))()
+grr
